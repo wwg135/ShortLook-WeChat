f5260d80eded7845e1ea5ecbae65bf6b7bd3f9f5
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-04-12 12:10:18 +0800

    Update WeChatContactPhotoProvider.m

--- WeChatContactPhotoProvider.m
+++ WeChatContactPhotoProvider.m
@@ -80,15 +80,19 @@
     return @"";
 }
 
-- (NSString *)md5:(NSString *)input {
-    const char *cStr = [input UTF8String];
-    unsigned char digest[CC_MD5_DIGEST_LENGTH];
-    CC_MD5( cStr, strlen(cStr), digest );
-    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
-    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
-        [output appendFormat:@"%02x", digest[i]];
+- (NSString *)sha256:(NSString *)input {
+  const char *cStr = [input UTF8String];
+  unsigned char digest[CC_SHA256_DIGEST_LENGTH];
+  NSData *data = [NSData dataWithBytes:cStr length:strlen(cStr)];
+  if (data) {
+    CC_SHA256(data.bytes, (CC_LONG)data.length, digest);
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH * 2];
+    for (NSInteger i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
+      [output appendFormat:@"%02x", digest[i]];
     }
     return output;
+  }
+  return nil;
 }
 
 @end
